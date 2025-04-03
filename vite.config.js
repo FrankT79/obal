5b79373641358371ea06f2ad0b17431b17eaa9c1
FrankT79 <tacsikf@gmail.com>
2025-04-03 15:56:48 +0200

    first try

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
- // base: "/Meta-capstone",
+  base: "/obal",
   plugins: [react()],
    test: {
     setupFiles: 'src/setupTests.js',
