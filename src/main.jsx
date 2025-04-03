c57e3302bcdef32f2883db80cdcb0aa28a8832fc
FrankT79 <tacsikf@gmail.com>
2025-04-03 15:40:56 +0200

    router fixed

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,10 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='/Meta-capstone'>
+       <BrowserRouter basename='/'>
     <App />
     </BrowserRouter>
   </StrictMode>,
